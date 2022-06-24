e20652b5c4b967f9a841bdf2daa156d71eec83b0
doncity-tech <princewill.opara@outlook.com>
2022-06-24 08:40:03 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// Build a basic arithmetic calculator without a frontend
+// The calculator should be able to perform basic operations like Addition, Subtraction, 
+// Multiplication, & Division
+
+const add = (a, b) => {
+  return a + b;
+}
+
+const subtract = (a, b) => {
+  return a - b;
+}
+
+const multiply = (a, b) => {
+  return a * b;
+}
+
+const divide = (a, b) => {
+  return a / b;
+}
